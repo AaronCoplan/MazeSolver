50b71744d31f05c7f331abf7f6bad8e500aa5da9
Aaron Coplan <afcoplan@gwu.edu>
2016-03-16 01:44:46 -0400

    Changes to Driver class.
    Updated the input for y and x, height and width. Also, removed todo list
    and moved it to the "issues" section on GitHub.

--- src/coplan/mazes/Driver.java
+++ src/coplan/mazes/Driver.java
@@ -8,8 +8,12 @@ public class Driver {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter x, the width of the maze:");
 		String xString = sc.nextLine();
+		System.out.println("Enter y, the height of the maze:");
 		String yString = sc.nextLine();
+		
+		sc.close();
 		
 		int x = Integer.parseInt(xString);
 		int y = Integer.parseInt(yString);
@@ -22,9 +26,6 @@ public class Driver {
 		
 		d.drawMaze(maze);
 		//d.drawSolution(maze);		
-		
-		//TODO: make mazes into complex mazes by opening up more holes in walls, which may generate loops
-		//TODO: solve mazes, both simple and complex
-		//TODO: Making this change to demonstrate a commit.
+
 	}
 }
