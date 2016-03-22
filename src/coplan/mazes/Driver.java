5e9130484f71e8d0650f67f4c9e27cccfc2e89aa
Aaron Coplan <afcoplan@gwu.edu>
2016-03-21 23:48:49 -0400

    Committing update to Driver class.

--- src/coplan/mazes/Driver.java
+++ src/coplan/mazes/Driver.java
@@ -5,6 +5,7 @@ public class Driver
 	public static void main(String[] args)
 	{	
 		//all events are initialized & carried out in the GUI	
+		//from here, everything happens on the swing thread or the action threads
 		new Display().setUp();	
 	}
 }
